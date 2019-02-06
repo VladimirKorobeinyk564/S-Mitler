1f7c8366098aba09778f6a353c6fde79bfcc4d00
Vladimir <vovakorobeinyk2001@gmail.com>
2019-02-06 22:45:58 +0300

    Creat header and header-menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
+	$('#my-menu').mmenu({
+  		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black',],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты S&Mitler" />'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
 
-	// Custom JS
+	var API = $("#my-menu").data("mmenu");
+	var $menuBtn = $("header .hamburger");
+	 
+	API.bind( "open:finish", function() {
+		$menuBtn.addClass('is-active');		
+	});
 
+	API.bind( "close:finish", function() {
+		$menuBtn.removeClass('is-active');		
+	});
 });
